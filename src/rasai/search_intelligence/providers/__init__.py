744788d0048feab4778892b94bb84814efd489b6
Jonathan R Cardozo <cardozo81@gmail.com>
2026-09-09 13:55:40 -0300

    Register SerpApi Bing adapter

--- src/rasai/search_intelligence/providers/__init__.py
+++ src/rasai/search_intelligence/providers/__init__.py
@@ -1,5 +1,6 @@
 """Concrete SERP provider adapters."""
 from .fixture import FixtureSerpProvider
 from .serpapi import SerpApiProvider
+from .serpapi_bing import SerpApiBingProvider
 
-__all__ = ["FixtureSerpProvider", "SerpApiProvider"]
+__all__ = ["FixtureSerpProvider", "SerpApiProvider", "SerpApiBingProvider"]
